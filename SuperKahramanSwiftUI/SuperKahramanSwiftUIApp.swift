f3dfa1c4dc99155b7f248b3666bd7a2d9fd2882d
Şirin Ekşi <sirin.eksi@hotmail.com>
2023-05-17 14:27:43 +0300

    Initial Commit

---
+++ SuperKahramanSwiftUI/SuperKahramanSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperKahramanSwiftUIApp.swift
+//  SuperKahramanSwiftUI
+//
+//  Created by ceksi on 17.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperKahramanSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
